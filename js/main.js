026aa17d61cdb98f5960698d3a260abac16b4807
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2023-02-14 09:12:47 +0000

    Add html, css and js file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const email = document.querySelector('#email').value
+const nhisNumber = document.querySelector('#nhis_number').value
+const clientNumber = document.querySelector('client_number').value
+const 
+
