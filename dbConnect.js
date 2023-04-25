c54b63c8042ba4b0abd67cfef4fbe2bfa4f1e4d2
afiamoah <35384642+afiamoah@users.noreply.github.com>
2023-04-25 08:23:14 +0100

    Update dbConnect.js

--- dbConnect.js
+++ dbConnect.js
@@ -2,10 +2,22 @@ import mysql from 'mysql'
 import { config } from 'dotenv'
 config()
  export const dbconnect=mysql.createConnection({
-        host:process.env.HOST,
-        user:process.env.USER,
-        password:process.env.PASSWORD,
-        database :process.env.DATABASE,
+        host:"familyplanning.cbg4o5mtlacu.us-east-1.rds.amazonaws.com",
+        user:"admin,
+        password:"ma5t3rpass",
+        database :"familyplanning",
         multipleStatements: true
     
 })
+ 
+//  import mysql from 'mysql'
+// import { config } from 'dotenv'
+// config()
+//  export const dbconnect=mysql.createConnection({
+//         host:process.env.HOST,
+//         user:process.env.USER,
+//         password:process.env.PASSWORD,
+//         database :process.env.DATABASE,
+//         multipleStatements: true
+    
+// })
